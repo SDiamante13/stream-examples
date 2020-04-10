4bda471af9af3f263c93d44208daf5ecade23566
Ritu Agrawal and Steven Diamante <pair+ritu+steven@pivotal.io>
2020-04-10 12:06:08 -0500

    Adding more examples.

--- java7-examples/src/main/java/tech/pathtoprogramming/java7examples/Examples7.java
+++ java7-examples/src/main/java/tech/pathtoprogramming/java7examples/Examples7.java
@@ -1,6 +1,14 @@
 package tech.pathtoprogramming.java7examples;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /***
@@ -8,19 +16,17 @@ import java.util.List;
  */
 public class Examples7 {
 
-    public static void main(String[] args) {
-        // 1. Multiple Each Item in a List by 2
-        List<Integer> items = new ArrayList<>();
+    public static void main(String[] args) throws URISyntaxException {
+
+        System.out.println("1. Multiple Each Item in a List by 2");
+        List<Integer> items = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
         List<Integer> result = new ArrayList<>();
-        for (int i = 1; i < 10; i++) {
-            items.add(i);
-        }
         for (int item: items) {
             result.add(item * 2);
         }
         System.out.println(result);
 
-        // 2. Sum a List of Numbers
+        System.out.println("2. Sum a List of Numbers");
         List<Integer> numbers = new ArrayList<>();
         int sum = 0;
 
@@ -31,6 +37,28 @@ public class Examples7 {
             sum += number;
         }
         System.out.println(sum);
+
+        System.out.println("3. Verify if Exists in a String");
+        final List<String> keywords = Arrays.asList("brown", "fox", "dog", "pangram");
+        final String tweet = "The quick brown fox jumps over a lazy dog.";
+        boolean tweetContainsAtLeastOneKeyword = false;
+        for (String s : keywords) {
+            if (tweet.contains(s)) {
+                tweetContainsAtLeastOneKeyword = true;
+                break;
+            }
+        }
+        System.out.println(tweetContainsAtLeastOneKeyword);
+
+        System.out.println("4. Read in a file");
+        Path path = Paths.get(Examples7.class.getClassLoader().getResource("data.txt").toURI());
+        try (BufferedReader reader = Files.newBufferedReader(path, Charset.defaultCharset())) {
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                System.out.println(line);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+        }
     }
 }
 
--- java8-examples/src/main/java/tech/pathtoprogramming/java8examples/Examples8.java
+++ java8-examples/src/main/java/tech/pathtoprogramming/java8examples/Examples8.java
@@ -1,7 +1,17 @@
 package tech.pathtoprogramming.java8examples;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toCollection;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.IntStream.range;
 
@@ -10,18 +20,35 @@ import static java.util.stream.IntStream.range;
  */
 public class Examples8 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException {
 
-        // 1. Multiple Each Item in a List by 2
+        System.out.println("1. Multiple Each Item in a List by 2");
         List<Integer> result = range(1, 10)
                 .map(i -> i * 2)
                 .boxed()
                 .collect(toList());
         System.out.println(result);
 
-        // 2. Sum a List of Numbers
+        System.out.println("2. Sum a List of Numbers");
         int sum = range(1, 1000).sum();
         int sum2 = range(1, 1000).reduce(0, Integer::sum);
-        System.out.printf("Sum using sum method: %d %nSum using reduce method: %d", sum, sum2);
+        System.out.printf("Sum using sum method: %d %nSum using reduce method: %d %n", sum, sum2);
+
+
+        System.out.println("3. Verify if Exists in a String");
+        final List<String> keywords = Arrays.asList("brown", "fox", "dog", "pangram");
+        final String tweet = "The quick brown fox jumps over a lazy dog.";
+        boolean tweetContainsAtLeastOneKeyword = keywords.stream()
+                .anyMatch(s -> tweet.contains(s));
+        System.out.println(tweetContainsAtLeastOneKeyword);
+
+        System.out.println("4. Read in a file");
+        Path path = Paths.get(Examples8.class.getClassLoader().getResource("data.txt").toURI());
+        try (Stream<String> lines = Files.lines(path, Charset.defaultCharset())) {
+            List<String> fileLines = lines.collect(toCollection(LinkedList::new));
+            System.out.println(fileLines);
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+        }
     }
 }
